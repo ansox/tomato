d3191716210e9d8ff260eb5ed3cf717de8b8ee5a
ansox <anderson.floriano@gmail.com>
2020-05-18 18:19:42 -0300

    Creating round timer

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -6,9 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
+  stroke: number;
+  timerValue: number;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    setTimeout(() => {
+      const time = 20; /* how long the timer will run (seconds) */
+      const initialOffset = 440;
+      let i = 1;
+
+      /* Need initial run as interval hasn't yet occured... */
+      this.stroke = initialOffset - (1 * (initialOffset / time));
+
+      const interval = setInterval(() => {
+        this.timerValue = i;
+        if (i === time) {
+          clearInterval(interval);
+          return;
+        }
+
+        this.stroke = initialOffset - ((i + 1) * (initialOffset / time));
+
+        i++;
+      }, 1000);
+
+    }, 0);
+  }
 
 }
