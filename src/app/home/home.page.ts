b2242e8136fc8a2a7f92cd3e3a69f0084f9452e4
ansox <anderson.floriano@gmail.com>
2020-05-18 11:34:16 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+// tslint:disable-next-line:component-class-suffix
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
 }
