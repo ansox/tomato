3a8b4ac630310322eafec079b05e016fe8d6dcda
ansox <anderson.floriano@gmail.com>
2020-05-20 17:44:38 -0300

    Created inputs and outputs in timer component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,8 @@ export class HomePage {
 
   constructor() { }
 
+  onEndOfTime() {
+    alert('fim');
+  }
+
 }
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -6,33 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
+  @Input() duration: number;
+  @Output() endOfTime: EventEmitter<any> = new EventEmitter<any>();
   stroke: number;
-  timerValue: number;
+  timerValue: string;
+  running = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.timerValue = this.getTimerValue(this.duration * 60);
+
+  }
+
+  start() {
+    if (this.running) {
+      return;
+    }
+
     setTimeout(() => {
-      const time = 20; /* how long the timer will run (seconds) */
+      this.running = true;
+
+      const timeTotal = this.duration * 60; /* how long the timer will run (seconds) */
       const initialOffset = 440;
-      let i = 1;
+      let fillIndex = 1;
 
       /* Need initial run as interval hasn't yet occured... */
-      this.stroke = initialOffset - (1 * (initialOffset / time));
+      this.stroke = initialOffset - (1 * (initialOffset / timeTotal));
+      this.timerValue = this.getTimerValue(timeTotal - fillIndex);
 
       const interval = setInterval(() => {
-        this.timerValue = i;
-        if (i === time) {
+        this.timerValue = this.getTimerValue(timeTotal - fillIndex);
+
+        if (fillIndex === timeTotal) {
           clearInterval(interval);
+          this.running = false;
+          this.endOfTime.emit();
           return;
         }
 
-        this.stroke = initialOffset - ((i + 1) * (initialOffset / time));
+        this.stroke = initialOffset - ((fillIndex + 1) * (initialOffset / timeTotal));
 
-        i++;
+        fillIndex++;
       }, 1000);
 
     }, 0);
   }
 
+  private getTimerValue(time: number): string {
+    const minutes = this.getMinutes(time);
+    const seconds = this.getSeconds(time);
+
+    return `${minutes}:${seconds}`;
+  }
+
+  private getMinutes(time: number): string {
+    const minutes = Math.floor(time / 60);
+    return `0${minutes}`.slice(-2);
+  }
+
+  private getSeconds(time: number): string {
+    const seconds = time % 60;
+    return `0${seconds}`.slice(-2);
+  }
 }
